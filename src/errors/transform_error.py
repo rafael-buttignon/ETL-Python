f207b108982141eaf7fae5fb7df7efb5d1f05cfa
Rafael Augusto <rafaaugustocontato@gmail.com>
2022-05-12 21:21:59 -0300

    feat: Finishing implementing transform stage

---
+++ src/errors/transform_error.py
@@ -0,0 +1,6 @@
+class TransformError(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.mesage = message
+        self.error_type = 'TransformError'
--- src/stages/tansform/transform_raw_data_test.py
+++ src/stages/tansform/transform_raw_data_test.py
@@ -1,6 +1,23 @@
+from src.errors.transform_error import TransformError
 from .transform_raw_data import TransformRawData
 from src.stages.extract.contracts.mocks.extract_contract import extract_contract_mock
+from src.stages.extract.contracts.transform_contract import TransformContract
 
 def test_transform():
     transform_raw_data = TransformRawData()
-    transformed_information = transform_raw_data.transform(extract_contract_mock)
+    transformed_data_contract = transform_raw_data.transform(extract_contract_mock)
+
+    assert isinstance(transformed_data_contract, TransformContract)
+    assert "first_name" in transformed_data_contract.load_content[0]
+    assert "artist_id" in transformed_data_contract.load_content[0]
+    assert "link" in transformed_data_contract.load_content[0]
+    assert "extraction_date" in transformed_data_contract.load_content[0]
+
+def test_transform_error():
+    transform_raw_data = TransformRawData()
+    
+    try:
+        transform_raw_data.transform('Entrada Com Erro')
+    except Exception as message:
+        assert isinstance(message, TransformError)
+
